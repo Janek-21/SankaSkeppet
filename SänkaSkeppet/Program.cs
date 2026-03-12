3ec81b3a3b0447ec5b0e3242a67bd6a47f7bf596
Janek-21 <szablewski.janek.9@gmail.com>
2026-03-12 09:16:03 +0100

    Training github

--- SänkaSkeppet/Program.cs
+++ SänkaSkeppet/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string message = "Update your GITHUB!!!!";
+            Console.WriteLine(message);
         }
     }
 }
